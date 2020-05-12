a7fc93f76ae3744aa6054be1606e212dc47bbb69
Keturah Howard <keturah.dev@gmail.com>
2020-05-12 14:04:10 -0700

    fix typo and plan out api call functions

--- catboard-interface-2/src/components/BoardList.js
+++ catboard-interface-2/src/components/BoardList.js
@@ -4,16 +4,28 @@ import PropTypes from 'prop-types';
 
 function BoardList() {
   // const [listofBoards, setBoardList] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
   
   // useEffect(() => {
   //   get();}, []
   // );
 
-  // useEffect( async () => {
-  // const result = await fetch('http://localhost:5000/api/boards, 
-  // method: 'GET',
-  // )}, []
-  // );
+//   useEffect(() => {
+//     fetch(
+        // 'http://localhost:5000/api/boards', 
+//     { 
+//     method: 'GET',
+//     })
+//   }
+// )
+// .then(res => res.json())
+//       .then(response => {
+//         setBoardList(response);
+//         setIsLoading(false);
+//       })
+//       .catch(error => console.log(error));
+//   }, []);
 
   return(
     <React.Fragment>
@@ -24,7 +36,8 @@ function BoardList() {
   )
 }
 
-BoardList.proptypes = {
+
+BoardList.propTypes = {
   name: PropTypes.string,
   boardId: PropTypes.number,
   description: PropTypes.string,
