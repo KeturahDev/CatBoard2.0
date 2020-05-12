fdcf2f1c9414f9408e8ca63b568820d705e81844
Keturah Howard <keturah.dev@gmail.com>
2020-05-12 14:02:56 -0700

    create empty component where form will be
    Co-authored-by: jeremy kale padot <jeremy@padot.us>
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>
    Co-authored-by: Rachel Schieferstein <violenza@gmail.com>

---
+++ catboard-interface-2/src/components/BoardForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function BoardForm() {
+  return(
+    <React.Fragment>
+      <div style={{border: "2px solid pink"}}>
+
+      <p>wow its a form</p>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default BoardForm;
