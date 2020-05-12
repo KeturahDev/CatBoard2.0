580d8e634da08d647529846445cebd9ce5f9ea1c
Keturah Howard <keturah.dev@gmail.com>
2020-05-12 16:14:02 -0700

    add POST functionality to boards
    Co-authored-by: jeremy kale padot <jeremy@padot.us>
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>
    Co-authored-by: Rachel Schieferstein <violenza@gmail.com>

--- catboard-interface-2/src/components/BoardForm.js
+++ catboard-interface-2/src/components/BoardForm.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function BoardForm() {
+function BoardForm(props) {
+
+  function handleApplyInputs (event ){
+    event.preventDefault();
+    const board = {
+      Name: event.target.Name.value,
+      Description: event.target.Description.value,
+      ImageURL: event.target.ImageURL.value
+    }
+    props.onAddingBoard(board);
+    props.onSubmitForm();
+  }
+
   return(
     <React.Fragment>
       <div style={{border: "2px solid pink"}}>
-
-      <p>wow its a form</p>
+        <form onSubmit={handleApplyInputs}>
+          <input type="text" name="Name" placeholder="board name"/>
+          <input type="text" name="Description" placeholder="describe board"/>
+          <input type="text" name="ImageURL" placeholder="image url for banner"/>
+          <button type="submit">submit</button>
+        </form>
       </div>
     </React.Fragment>
   );
+}
+
+BoardForm.propTypes = {
+  onAddingBoard: PropTypes.func,
+  onSubmitForm: PropTypes.func
 }
 
 export default BoardForm;
--- catboard-interface-2/src/components/BoardView.js
+++ catboard-interface-2/src/components/BoardView.js
@@ -1,29 +1,46 @@
 import React, {useState} from 'react';
-import BoardList from './BoardList'
-import BoardForm from "./BoardForm"
+import BoardList from './BoardList';
+import BoardForm from "./BoardForm";
+import PostView from './PostView';
 // import Board from "./Board"
 
 function BoardView(){
 
 // hook toggles between board list and board form
 const [currentlyVisible, setVisibleComponent] = useState("List");
+const [selectedBoard, setBoard] = useState(null);
+
+  async function AddBoard(newBoard){
+    await fetch(`http://localhost:5000/api/boards`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newBoard)
+    });
+  }
 
   // function setting 'currentlyVisibleCOmponent' based on state
   const setVisible =  () => {
-
+    if(selectedBoard != null) {
+      return {
+        component: <PostView />,
+        buttontext: ""
+      }
+     } else {
     if(currentlyVisible === "List") {
       return {
-       component: <BoardList />,
+       component: <BoardList  />,
        buttontext: "AddBoard"
       } 
     } else {
       return {
-        component: <BoardForm />,
+        component: <BoardForm onSubmitForm={handleSetVisibleComponent} onAddingBoard={AddBoard} />,
         buttontext: "Back"
        } 
     }
   }
-
+    
   const handleSetVisibleComponent = () => {
     if (currentlyVisible === "List") {
       setVisibleComponent("Form")
@@ -32,13 +49,14 @@ const [currentlyVisible, setVisibleComponent] = useState("List");
     }
   }
 
-  const currentlyVisibleComponent = setVisible()
-  return(
-    <React.Fragment>
-      {currentlyVisibleComponent.component}
-      <button onClick={handleSetVisibleComponent}>{currentlyVisibleComponent.buttontext}</button>
-    </React.Fragment>
-  );
-}
 
-export default BoardView
+    const currentlyVisibleComponent = setVisible()
+    return(
+      <React.Fragment>
+        {currentlyVisibleComponent.component}
+        <button onClick={handleSetVisibleComponent}>{currentlyVisibleComponent.buttontext}</button>
+      </React.Fragment>
+    );
+  }
+}
+export default BoardView;
