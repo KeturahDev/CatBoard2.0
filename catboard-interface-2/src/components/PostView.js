0d3a49f2ff0ea0998dd2cbd326ba8e01590e6003
Keturah Howard <keturah.dev@gmail.com>
2020-05-13 22:06:25 -0700

    attempt to achieve select-single-post functionality
    Co-authored-by: violenzae <violenza@gmail.com>
    Co-authored-by: Jeremy Kale Padot <jeremy@padot.us>
    Co-authored-by: Patrick Delaney <Prestwick97@gmail.com>

--- catboard-interface-2/src/components/PostView.js
+++ catboard-interface-2/src/components/PostView.js
@@ -6,7 +6,8 @@ import PostDetails from './PostDetails';
 import PropTypes from 'prop-types'
 
 function PostView(props) {
-
+  const [selectedPostId, selectPostId] = useState(null)
+  const [currentlyVisible, setCurrentlyVisible] = useState("BoardDetails")
   const [view, setView] = useState(<p>Loading...</p>);
 
   useEffect(() => {
@@ -21,18 +22,41 @@ function PostView(props) {
     })
       .then(res => res.json())
       .then(jsonifiedResponse => {
-        setView(<BoardDetails board={jsonifiedResponse}/>)
+        setView(<BoardDetails board={jsonifiedResponse} postId={selectedPostId} onSelectingPost={selectingPost}/>)
       })
       .catch(error =>
         console.log(error)) 
   }
- 
 
+  const selectingPost = (postid) => {
+    selectPostId(postid)
+    setCurrentlyVisible("BoardForm")
+    console.log(postid)
+  }
+
+  const setVisibility = () => {
+    console.log("SELECTEDPOST: ", selectedPostId)
+    if (selectedPostId !== null) {
+      return {
+        component: <PostDetails />,
+        buttontext: "Back to the Board"
+      }
+    } else if (currentlyVisible === "BoardDetails") {
+      return {view}
+    } else {
+      return {
+        component: <PostForm />,
+        buttontext: "Back to the Board"
+      }
+    }
+  }
+ 
+  const currentlyVisComponent = setVisibility()
   return (
     <React.Fragment>
       <div style={{border: "2px solid green"}}>
         <div>
-          {view}
+          {currentlyVisComponent.component}
         </div>
       </div>
     </React.Fragment>
